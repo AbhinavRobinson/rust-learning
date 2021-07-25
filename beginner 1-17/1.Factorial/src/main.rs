d3db5d9ac086d1d8a6a1c276b254be77068a51ff
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-07-25 14:36:37 +0530

    (feat): add factorial

--- beginner 1-17/1.Factorial/src/main.rs
+++ beginner 1-17/1.Factorial/src/main.rs
@@ -1,9 +1,24 @@
 use std::io;
 
 fn main() {
-    println!("Hi! Enter your name!");
+    // prompt
+    println!("Enter number to find factorial:");
 
+    // get and parse number
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Error: Unable to parse user input.");
-    println!("Hello, {}!",&input.trim());
+    let n:i64 = input.trim().parse().expect("Error: Not a number.");
+
+    // find factorial! max(20)
+    if n > 20 {println!("Cannot be larger than 20")}
+    else {println!("{}! = {}",&n,factorial(&n))} 
+}
+
+fn factorial(n: &i64) -> i64 {
+    // if less than 1
+    if *n <= 0 { return 0 };
+    // if 1
+    if *n == 1 { return 1 };
+    // else n * n-1
+    *n * factorial(&(n-1))
 }
